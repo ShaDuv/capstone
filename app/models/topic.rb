daab01453e810f4da5ce5f10c2b7341202f11f1c
Shawna Duvall <shawnaduvall@gmail.com@>
2019-09-20 16:51:34 -0700

    places start of if statement"

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,8 @@
 class Topic < ApplicationRecord
   has_many :datas
+
   def find_by_name()
-  end
+  # will need to add a function here that will search the database to see if the term is already there and if so pull from that if not then pull from scraper.
+  # end
+
 end
